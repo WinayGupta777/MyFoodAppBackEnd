192490fbbd909bb3218527f636a5248940faa7f1
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-03-27 09:12:38 +0530

    UserDataModel in MongoDB created with UserData_FoodApp Schema;

--- models/UserDataModel.js
+++ models/UserDataModel.js
@@ -4,3 +4,24 @@ const mongo = require("mongoose");
 mongo.connect(file.link)
 .then(()=>console.log("MongoDB connected"))
 .catch((e)=>console.log("Mongo connection error: "+ e));
+
+const UserDataModel = mongo.model("UserData_FoodApp",
+    mongo.Schema({
+        username:{
+            type: String,
+            required:[true, "Username required"]
+        },
+        email:{
+            type: String,
+            unique:true,
+            required:[true, "EmailRequired"],
+        },
+        password:{
+            type: String,
+            minlength: [5,"Password must be atleast 6"],
+            required:[true, "PasswordRequired"]
+        }
+    })
+);
+
+module.exports = UserDataModel;
