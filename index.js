163606dc5b360cbe141055c8f41e1078063d8387
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-03-27 09:37:37 +0530

    SignUp Module done; Required Validations applied; Error catching applied;

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
+const routeSignUp = require("./routes/signup");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.use("/",routeSignUp);
 
 app.listen(8080, console.log("Server Started"));
--- routes/signup.js
+++ routes/signup.js
@@ -1,2 +1,34 @@
 const express = require('express');
+const UserDataModel = require("../models/UserDataModel");
 const router = express.Router();
+
+
+router.post("/signup", async (req,res)=>{
+    console.log("Request for user creation received!!");
+    const userdata = req.body;
+    console.log(userdata);
+    const createUser = new UserDataModel((
+        {
+            username: userdata.username,
+            email:userdata.email, 
+            password:userdata.password 
+        }
+    ));
+
+    await createUser.save()
+    .then(()=>res.send({status:true,message:"Account created successfully"}))
+    .catch((err)=>{
+        if(err.message.includes("E11000")){
+            res.send({status:false,message:"This email is already exists"});
+        }
+        if(err.message.includes("atleast")){
+            res.send({status:false,message:"Password should be atleast 5 character long"});
+        }
+        if(err.message.includes("EmailRequired")){
+            res.send({status:false,message:"EmailRequired"});
+        }
+        console.log(err);
+    });
+})
+
+module.exports = router;
